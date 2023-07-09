20d49705ae0c17cdb7d901c835946a15fecd1ffd
Gyuni <gyuni@soongsil.ac.kr>
2023-07-09 15:26:29 +0900

    Create Project

---
+++ DontForget/DontForgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontForgetApp.swift
+//  DontForget
+//
+//  Created by Gyuni on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct DontForgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
